2021918690ab357b2b5e6c52cbdbfc2ad757c8ab
Aranga Sakthivel R <157717490+arangasakthivel@users.noreply.github.com>
2025-07-26 12:14:01 +0530

    initial commit

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,100 @@
+import express from "express";
+import User from "../models/User.js"; // Assuming you have a User model defined
+import jwt from "jsonwebtoken";
+
+const router=express.Router();
+
+const generateToken = (userId) => {
+    return jwt.sign({userId},process.env.JWT_SECRET, {expiresIn:"15d"});
+}
+
+router.post("/register",async(req,res)=>{
+    try {
+        const{email,username,password}=req.body;
+
+        if(!username || !email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+
+        if(password.length < 6){
+            return res.status(400).json({message:"Password must be at least 6 characters long"});
+        }
+
+        if(username.length<3){
+            return res.status(400).json({message:"Username must be at least 3 characters long"});
+        }
+
+        // check if user already exists
+        const existingEmail=await User.findOne({email});
+        if(existingEmail){
+            return res.status(400).json({message:"Email already exists"});
+        }
+        const existingUsername=await User.findOne({username});  
+        if(existingUsername){
+            return res.status(400).json({message:"Username already exists"});
+        }
+
+        // get random avatar
+        const profileImage=`https://api.dicebar.com/7.x/avataaars/svg?seed=${username}`;
+
+        // create new user  
+        const user=new User({
+            email,
+            username,
+            password,
+            profileImage,
+        });
+        
+        await user.save();
+        
+        const token= generateToken(user._id);
+
+        res.status(201).json({
+            token,
+            user:{
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                profileImage: user.profileImage,  
+            },
+        });
+        
+    } catch (error) {
+        console.log("Error in the registration route:", error);
+        res.status(500).json({message:"Internal server error"});
+        
+    }
+});
+
+router.post("/login",async(req,res)=>{
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password) return res.status(400).json({message:"All fields are required"});
+        // check if user exists
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message:"Invalid credentials"});
+
+        // check if password is correct
+        const isPasswordValid = await user.comparePassword(password);
+        if(!isPasswordValid) return res.status(400).json({message:"Invalid credentials"});
+
+        const token = generateToken(user._id);
+        res.status(200).json({
+            token,
+            user: {
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                profileImage: user.profileImage,
+            },
+        });
+    } catch (error) {
+        console.log("Error in the login route:", error);
+        res.status(500).json({message:"Internal server error"});
+    }
+});
+
+
+
+export default router;
